46fc860674d1de4406e5de3a3f3961d1aefcb53c
Gizot <tastangizem@gmail.com>
2023-07-14 00:08:21 +0300

    user-list done/must be checked aftercategory&posts

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-list',
@@ -6,5 +8,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
+  users: User[] = [];
+  userId : number = 0;
+  username : string = "";
+  email : string = "";
+  creationDate : string = "";
+  isActive : boolean = false;
+  editMode : boolean = false;
 
-}
+
+  constructor(private userService : UserService) {
+    if (this.userService.getUsers().length === 0)
+    this.userService.setUsers();
+    else
+    this.users = this.userService.getUsers();
+  }
+  handleEditClick($event: number): void {
+    this.editMode = true;
+    this.userId = $event;
+  }
+
+  handleSaveClick() {
+    if(this.username == '' || this.email == '' || this.creationDate == '')
+      alert("All the empty spaces must be filled!");
+    else if(this.userService.checkOfUsersIsUnique(this.username,this.email,this.userId) === false)
+      alert("This email address/username already exist! Please enter another email address/username ");
+    else {
+      const user: User = {
+        userId: this.userId,
+        username: this.username ,
+        email: this.email ,
+        creationDate: this.creationDate ,
+        isActive: this.isActive
+      }
+      this.userService.editUser(user, this.userId);
+      this.users = this.userService.getUsers();
+      this.handleCancelClick();
+    }
+    
+
+  }
+
+  
+  handleCancelClick(): void {
+    this.editMode= false;
+    this.username = "";
+    this.email = "";
+    this.creationDate = "";
+    this.userId= 0;
+
+  }
+  
+  
+
+  }
+  
+
+
+
+
+
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -17,7 +17,46 @@ export class UserService {
   getUsers() : User[] {
     return this.users;
   }
+  createUser(user : User) : void {
+    this.users.push(user);
+  }
+  editUser (editUser: User, id : number) : void {
+    this.users = this.users.map(user=> {
+      if(user.userId === id)
+      user = editUser
+      return user;
+    })
+  }
+
+  deleteUser(id : number): void {
+    this.users = this.users.filter((user) => user.userId !== id);
+  }
+
+  findById(id : number): User | undefined {
+    return this.users.find((user)=> user.userId === Number(id)) 
+  }
+  findByUserName(username : string) : User | undefined {
+    return this.users.find((user)=> user.username === username)
+  }
+  findByEmail (email : string) : User | undefined {
+    return this.users.find((user)=> user.email === email)
+  }
+  checkOfUsersIsUnique(username : string, email : string, id : number): boolean {
+    if(this.users.find((user)=> user.username === username.toLowerCase()) !== undefined
+    &&this.findByUserName(username.toLowerCase())!.userId !== id)
+    return false ;
+    else if (this.users.find((user)=>user.email === email.toLowerCase()) !== undefined 
+    && this.findByEmail(email.toLowerCase())!.userId !== id)
+      return false ;
+    else
+      return true ;
+  }
+  
+  }
+
+
+
 
   
 
-}
+
