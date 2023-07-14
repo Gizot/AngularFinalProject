c16b7f561b27ef59d852ba1179102810d097c2ce
Gizot <tastangizem@gmail.com>
2023-07-14 18:10:45 +0300

    post service done

--- src/app/post/post.service.ts
+++ src/app/post/post.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Post } from './post';
+import { defaultpost } from 'src/assets/defaultpost';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  private posts: Post[] = [];
 
   constructor() { }
+
+  getPost() : Post[] {
+    return this.posts;
+  }
+  setPost() : void {
+    this.posts = defaultpost;
+  }
+  deletePost(id : number) : void {
+    this.posts = this.posts.filter((post)=> post.postId !==id);
+  }
+  addPost(post : Post) {
+    this.posts.push(post);
+  }
+  editPost(editedPost : Post) {
+    this.posts = this.posts.map(post => {
+      if (post.postId === editedPost.postId)
+      post = editedPost;
+      return post;
+    });
+  }
+  findPostById(id : number) : Post | undefined {
+    return this.posts.find((post)=> Number(post.postId) === Number(id))
+  }
+
 }
