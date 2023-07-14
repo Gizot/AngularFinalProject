eedf71ea5fb9c28b1ead4e324044806831715fa1
Gizot <tastangizem@gmail.com>
2023-07-14 23:27:37 +0300

    categories.module added in app.route.module

--- src/app/categories/categories-service.service.ts
+++ src/app/categories/categories-service.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { PostService } from '../post/post.service';
+import { Category } from './category';
+import { defaultcategory } from 'src/assets/categoriesdefault';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesServiceService {
+  private category : Category[] = [];
 
-  constructor() { }
+  constructor(private postService : PostService) { }
+
+  getCategories(): Category[] {
+    return this.category;
+  }
+
+  setCategory() : void {
+    this.category = defaultcategory;
+  }
+  deleteCategory($event: number) {
+    if (this.postService.getPost().length === 0)
+      this.postService.setPost();
+    if (this.postService.getPost().filter(post => Number(post.categoryId) === Number($event)).length > 0)
+      alert("You cannot delete a category with posts");
+    else
+      this.category = this.category.filter(category => category.categoryId !== $event);
+  }
+  getById(id: number): Category | undefined {
+    return this.category.find((category) => category.categoryId === id);
+  }
+  editCategory(updateCategory: Category) {
+    this.category = this.category.map(category => {
+      if (category.categoryId === updateCategory.categoryId)
+        category = updateCategory;
+      return category;
+    });
+  }
+  checkUniqueCategory(name: string, id: number): boolean {
+    if(this.category.find((category)=> category.name === name.toLowerCase()) !== undefined
+    && this.findByName(name.toLowerCase())!.categoryId !== id)
+      return false ;
+    else
+      return true ;
+  }
+  addCategory(newName: string, newDate: string)
+  {
+    const newCategory: Category = {
+      categoryId: this.getCategories()[this.getCategories().length - 1].categoryId + 1,
+      name: newName,
+      creationDate: newDate,
+    }
+    if (this.checkUniqueCategory(newCategory.name, newCategory.categoryId) === true)
+      this.category.push(newCategory);
+    else
+      alert("This category already exist");
+  }
+
+  findByName(name: string) {
+    return this.category.find((category)=> category.name === name )
+  }
 }
--- src/assets/categoriesdefault.ts
+++ src/assets/categoriesdefault.ts
@@ -1,69 +1,69 @@
-export const defaultallcategories = [{
+export const defaultcategory = [{
     "categoryId": 1,
-    "name": "Rhamnaceae",
+    "name": "Çam",
     "creationDate": "2022-04-03"
   }, {
     "categoryId": 2,
-    "name": "Fabaceae",
+    "name": "Lavanta",
     "creationDate": "2021-12-30"
   }, {
     "categoryId": 3,
-    "name": "Lamiaceae",
+    "name": "Mimoza",
     "creationDate": "2017-10-18"
   }, {
     "categoryId": 4,
-    "name": "Rosaceae",
+    "name": "Lale",
     "creationDate": "2017-05-24"
   }, {
     "categoryId": 5,
-    "name": "Onagraceae",
+    "name": "Mercan Köşk",
     "creationDate": "2022-07-08"
   }, {
     "categoryId": 6,
-    "name": "Malvaceae",
+    "name": "Rosemarry",
     "creationDate": "2021-10-14"
   }, {
     "categoryId": 7,
-    "name": "Fumariaceae",
+    "name": "Camomile",
     "creationDate": "2019-06-12"
   }, {
     "categoryId": 8,
-    "name": "Rhamnaceae",
+    "name": "Sardunya",
     "creationDate": "2019-11-14"
   }, {
     "categoryId": 9,
-    "name": "Flacourtiaceae",
+    "name": "Kalonşe",
     "creationDate": "2017-04-30"
   }, {
     "categoryId": 10,
-    "name": "Asteraceae",
+    "name": "Zinya",
     "creationDate": "2019-12-05"
   }, {
     "categoryId": 11,
-    "name": "Lamiaceae",
+    "name": "Sardunya",
     "creationDate": "2020-08-21"
   }, {
     "categoryId": 12,
-    "name": "Garryaceae",
+    "name": "Gardenya",
     "creationDate": "2018-11-02"
   }, {
     "categoryId": 13,
-    "name": "Physciaceae",
+    "name": "Açelya",
     "creationDate": "2021-09-23"
   }, {
     "categoryId": 14,
-    "name": "Fabaceae",
+    "name": "Gül",
     "creationDate": "2020-09-19"
   }, {
     "categoryId": 15,
-    "name": "Fabaceae",
+    "name": "Limon",
     "creationDate": "2022-03-31"
   }, {
     "categoryId": 16,
-    "name": "Asteraceae",
+    "name": "Kaktüs",
     "creationDate": "2019-03-12"
   }, {
     "categoryId": 17,
-    "name": "Commelinaceae",
+    "name": "Skulent",
     "creationDate": "2017-02-27"
   }]
