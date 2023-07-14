ab1d52a27eb8bd8d6cdc0d45ddc00b743d0aa5b9
Gizot <tastangizem@gmail.com>
2023-07-14 19:33:51 +0300

    app routing tunning post list and post add created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserCreateComponent } from './user/user-create/user-create.component';
 import { UserListComponent } from './user/user-list/user-list.component';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { PostAddComponent } from './post/post-add/post-add.component';
+import { PostDetailComponent } from './post/post-detail/post-detail.component';
 
 
 const routes: Routes = [
   {path: "usercreate", component: UserCreateComponent},
   {path : "userlist", component : UserListComponent},
+  {path : "postlist", component : PostListComponent},
+  {path : "postadd", component : PostAddComponent},
+  {path : "postdetail", component : PostDetailComponent},
   
 ];
 
--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Post } from '../post';
+import { PostService } from '../post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
@@ -9,7 +11,27 @@ import { Post } from '../post';
 export class PostListComponent {
   posts: Post [] = [];
 
+  constructor(private postService : PostService, private router : Router) {
+    if(this.postService.getPost().length === 0)
+    this.postService.setPost();
+    this.posts = this.postService.getPost();
+  }
+
+  handleDeleteClick($event : number) : void {
+    this.postService.deletePost($event);
+    this.posts = this.postService.getPost();
+  }
   
+  handleDetailClick($event : number) : void {
+    this.router.navigate(["/postlist/" , $event]);
+  }
+
+
+
+
+  
+
+
 
   
 
