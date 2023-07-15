dc5cb4e09524c51d48f1819ae5418d548b47e019
Gizot <tastangizem@gmail.com>
2023-07-15 13:02:30 +0300

    default repository done

---
+++ src/assets/defaultcomment.ts
@@ -0,0 +1,402 @@
+export const defaultcomments = [
+    {
+      commentId: 1,
+      postId: 23,
+      userId: 7,
+      comment: "Güneş sever",
+      creationDate: "2019-06-28",
+      isConfirmed: false
+    },
+    {
+      commentId: 2,
+      postId: 15,
+      userId: 3,
+      comment: "Gölge sever",
+      creationDate: "2017-10-22",
+      isConfirmed: true
+    },
+    {
+      commentId: 3,
+      postId: 12,
+      userId: 2,
+      comment: "Sudan hoşlanır",
+      creationDate: "2019-10-14",
+      isConfirmed: true
+    },
+    {
+      commentId: 4,
+      postId: 7,
+      userId: 8,
+      comment: "Sudan nefret eder",
+      creationDate: "2017-11-25",
+      isConfirmed: true
+    },
+    {
+      commentId: 5,
+      postId: 100,
+      userId: 3,
+      comment: "Kokusu çok yoğun",
+      creationDate: "2018-12-07",
+      isConfirmed: false
+    },
+    {
+      commentId: 6,
+      postId: 81,
+      userId: 4,
+      comment: "Yaprakları tabakayla kaplı",
+      creationDate: "2018-06-07",
+      isConfirmed: true
+    },
+    {
+      commentId: 7,
+      postId: 16,
+      userId: 2,
+      comment: "Bu kaktüslere bayılıyorum",
+      creationDate: "2016-12-28",
+      isConfirmed: true
+    },
+    {
+      commentId: 8,
+      postId: 48,
+      userId: 6,
+      comment: "Sanırım bitkim soğuktan etkilendi",
+      creationDate: "2019-01-02",
+      isConfirmed: false
+    },
+    {
+      commentId: 9,
+      postId: 54,
+      userId: 2,
+      comment: "Bu çiçekten elinde olan var mı ?",
+      creationDate: "2018-10-27",
+      isConfirmed: false
+    },
+    {
+      commentId: 10,
+      postId: 25,
+      userId: 2,
+      comment: "Dikenlerine dikkat!",
+      creationDate: "2017-02-26",
+      isConfirmed: false
+    },
+    {
+      commentId: 11,
+      postId: 57,
+      userId: 2,
+      comment: "Çiçeğim soluyor ne yapmalıyım?",
+      creationDate: "2019-07-08",
+      isConfirmed: false
+    },
+    {
+      commentId: 12,
+      postId: 97,
+      userId: 4,
+      comment: "Doktora götür",
+      creationDate: "2019-08-27",
+      isConfirmed: true
+    },
+    {
+      commentId: 13,
+      postId: 79,
+      userId: 6,
+      comment: "Bana getir",
+      creationDate: "2020-05-20",
+      isConfirmed: false
+    },
+    {
+      commentId: 14,
+      postId: 21,
+      userId: 3,
+      comment: "Çicek satıyorum",
+      creationDate: "2019-03-19",
+      isConfirmed: true
+    },
+    {
+      commentId: 15,
+      postId: 8,
+      userId: 1,
+      comment: "Kırmızı",
+      creationDate: "2016-09-29",
+      isConfirmed: true
+    },
+    {
+      commentId: 16,
+      postId: 63,
+      userId: 1,
+      comment: "Mavi",
+      creationDate: "2018-09-24",
+      isConfirmed: true
+    },
+    {
+      commentId: 17,
+      postId:73,
+      userId: 5,
+      comment: "Yeşil",
+      creationDate: "2018-03-27",
+      isConfirmed: true
+    },
+    {
+      commentId: 18,
+      postId:11,
+      userId: 10,
+      comment: "Turuncu",
+      creationDate: "2018-06-25",
+      isConfirmed: true
+    },
+    {
+      commentId: 19,
+      postId: 77,
+      userId: 6,
+      comment: "Siyah",
+      creationDate: "2016-07-18",
+      isConfirmed: false
+    },
+    {
+      commentId: 20,
+      postId: 87,
+      userId: 3,
+      comment: "Soğanlı",
+      creationDate: "2019-04-25",
+      isConfirmed: false
+    },
+    {
+      commentId: 21,
+      postId: 31,
+      userId: 9,
+      comment: "Saçak köklü",
+      creationDate: "2020-05-05",
+      isConfirmed: false
+    },
+    {
+      commentId: 22,
+      postId: 92,
+      userId: 9,
+      comment: "Kazık kök",
+      creationDate: "2018-02-03",
+      isConfirmed: false
+    },
+    {
+      commentId: 23,
+      postId: 43,
+      userId: 5,
+      comment: "Taç yaprak",
+      creationDate: "2019-09-26",
+      isConfirmed: false
+    },
+    {
+      commentId: 24,
+      postId: 51,
+      userId: 3,
+      comment: "Aloe veragillerden",
+      creationDate: "2017-05-06",
+      isConfirmed: false
+    },
+    {
+      commentId: 25,
+      postId: 79,
+      userId: 10,
+      comment: "Sarma yapmalık yaprak",
+      creationDate: "2016-10-29",
+      isConfirmed: true
+    },
+    {
+      commentId: 26,
+      postId: 93,
+      userId: 6,
+      comment: "Asmagiller",
+      creationDate: "2019-04-09",
+      isConfirmed: false
+    },
+    {
+      commentId: 27,
+      postId:96,
+      userId: 2,
+      comment: "Kivi bir asmadır",
+      creationDate: "2017-02-03",
+      isConfirmed: false
+    },
+    {
+      commentId: 28,
+      postId: 24,
+      userId: 10,
+      comment: "Kivi ağacı yoktur",
+      creationDate: "2017-03-28",
+      isConfirmed: true
+    },
+    {
+      commentId: 29,
+      postId: 29,
+      userId: 1,
+      comment: "Varsa o başka bişeydir",
+      creationDate: "2020-04-03",
+      isConfirmed: false
+    },
+    {
+      commentId: 30,
+      postId: 70,
+      userId: 8,
+      comment: "Ceviz kabuğu boyar",
+      creationDate: "2020-02-27",
+      isConfirmed: false
+    },
+    {
+      commentId: 31,
+      postId: 45,
+      userId: 10,
+      comment: "Sendeki helikobakter pliori",
+      creationDate: "2017-05-17",
+      isConfirmed: true
+    },
+    {
+      commentId: 32,
+      postId: 3,
+      userId: 1,
+      comment: "Santa monika cinsi",
+      creationDate: "2020-03-17",
+      isConfirmed: false
+    },
+    {
+      commentId: 33,
+      postId: 2,
+      userId: 2,
+      comment: "Boş yapmayı özlemişim",
+      creationDate: "2017-05-24",
+      isConfirmed: false
+    },
+    {
+      commentId: 34,
+      postId: 58,
+      userId: 3,
+      comment: "Karamel makiiiYatoooo",
+      creationDate: "2019-08-10",
+      isConfirmed: true
+    },
+    {
+      commentId: 35,
+      postId: 18,
+      userId: 4,
+      comment: "Şeftali kokusu",
+      creationDate: "2019-09-28",
+      isConfirmed: false
+    },
+    {
+      commentId: 36,
+      postId: 19,
+      userId: 4,
+      comment: "O ne öyle incir reçeli gibi",
+      creationDate: "2017-02-21",
+      isConfirmed: false
+    },
+    {
+      commentId: 37,
+      postId: 20,
+      userId: 9,
+      comment: "Bu gerçek bir dram",
+      creationDate: "2016-12-14",
+      isConfirmed: true
+    },
+    {
+      commentId: 38,
+      postId: 21,
+      userId: 5,
+      comment: "Dram olan ne?",
+      creationDate: "2017-05-12",
+      isConfirmed: true
+    },
+    {
+      commentId: 39,
+      postId: 45,
+      userId: 1,
+      comment: "Reçele yapışan sinek",
+      creationDate: "2017-03-09",
+      isConfirmed: false
+    },
+    {
+      commentId: 40,
+      postId: 85,
+      userId: 6,
+      comment: "Bu çiçeğin suratı var gibi",
+      creationDate: "2017-12-30",
+      isConfirmed: false
+    },
+    {
+      commentId: 41,
+      postId: 16,
+      userId: 10,
+      comment: "Evet benim kaktüründe bacakları var ",
+      creationDate: "2020-01-16",
+      isConfirmed: false
+    },
+    {
+      commentId: 42,
+      postId: 14,
+      userId: 8,
+      comment: "sulamayı kes",
+      creationDate: "2016-07-23",
+      isConfirmed: true
+    },
+    {
+      commentId: 43,
+      postId: 35,
+      userId: 10,
+      comment: "Ellerinde bitleri imha et",
+      creationDate: "2016-08-30",
+      isConfirmed: false
+    },
+    {
+      commentId: 44,
+      postId: 53,
+      userId: 8,
+      comment: "Arap sabunu en iyisi",
+      creationDate: "2019-12-22",
+      isConfirmed: true
+    },
+    {
+      commentId: 45,
+      postId: 36,
+      userId: 1,
+      comment: "Acı biberde at biraz suya",
+      creationDate: "2018-03-27",
+      isConfirmed: true
+    },
+    {
+      commentId: 46,
+      postId: 39,
+      userId: 7,
+      comment: "soğanda doğrayayım mı ?",
+      creationDate: "2018-01-11",
+      isConfirmed: true
+    },
+    {
+      commentId: 47,
+      postId: 77,
+      userId: 8,
+      comment: "Yok ozaman ilaç değil yemek olur ",
+      creationDate: "2020-05-15",
+      isConfirmed: false
+    },
+    {
+      commentId: 48,
+      postId: 34,
+      userId: 9,
+      comment: "Barış Akarsu'yu analım",
+      creationDate: "2016-08-13",
+      isConfirmed: true
+    },
+    {
+      commentId: 49,
+      postId: 69,
+      userId: 2,
+      comment: "Güneş seven bir bitki ",
+      creationDate: "2016-09-20",
+      isConfirmed: false
+    },
+    {
+      commentId: 50,
+      postId: 5,
+      userId: 3,
+      comment: "Kırmızı çiçekleri var adı ne bunun ? ",
+      creationDate: "2018-12-24",
+      isConfirmed: true
+    }
+  ]
